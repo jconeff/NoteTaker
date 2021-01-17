111f5692f4bb548290b316d3b623e5a64fb36e9b
jconeff <jessicaconeff@gmail.com>
2021-01-17 14:34:39 -0500

    first commit

---
+++ routes/apiRoutes/noteRoutes.js
@@ -0,0 +1,37 @@
+const router = require('express').Router();
+const { notes } = require('../../db/db')
+const { v4: uuidv4 } = require('uuid');
+const { createNote, removeNote, editNote, findById } = require('../../lib/notes');
+
+
+router.get('/notes', (req, res) => {
+    res.json(notes)
+});
+
+router.get('/notes/:id', (req, res) => {
+    const result = findById(req.params.id, notes);
+    if (result) {
+        res.json(result)
+    } else {
+        res.sendStatus(404);
+    }
+})
+
+router.post('/notes', (req, res) => {
+    if (!req.body.id) {
+        req.body.id = uuidv4();
+        createNote(req.body, notes);
+    } else {
+        editNote(req.body, notes);
+    }
+    res.json(req.body)
+})
+
+router.delete('/notes/:id', (req, res) => {
+    const result = findById(req.params.id, notes);
+
+    removeNote(result, notes);
+    res.json();
+})
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+app.use(express.static('public'));
+
+// parse POST data
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Use apiRoutes
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
